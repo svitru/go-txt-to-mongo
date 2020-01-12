d73e0182190735ee803d3515bbf8bf0ed9ccfe14
С.В <sv@sv-it.ru>
2020-01-12 20:52:38 +0000

    add struct to mongo

--- tomongo.go
+++ tomongo.go
@@ -4,11 +4,11 @@ import (
     "context"
     "fmt"
     "log"
-//    "bufio"
-//    "os"
+    "bufio"
+    "os"
 //    "strconv"
-//    "strings"
-
+    "strings"
+    "reflect"
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -48,27 +48,42 @@ func main() {
 
     collection := client.Database("test").Collection("structs")
 
-    b := Words{3, "boo", []Dicts{{"one", "two"}, {"three", "four"}}}
-    _ , err = collection.InsertOne(context.TODO(), b)
-      if err != nil {
-        log.Fatal(err)
-      }
+    fmt.Println(reflect.TypeOf(collection))
 
-/*    file, err := os.Open("data.txt")
+    file, err := os.Open("tail.txt")
     if err != nil {
         log.Fatal(err)
     }
     defer file.Close()
 
     scanner := bufio.NewScanner(file)
+
+    s := []string{}
     for scanner.Scan() {
-      res := strings.Split(scanner.Text(), ";")
-      i, _ := strconv.Atoi(res[2])
-      arr := Words{res[0], res[1], i}
-      _ , err := collection.InsertOne(context.TODO(), arr)
-      if err != nil {
-        log.Fatal(err)
+      sline := scanner.Text()
+      if len(sline) > 1 {
+        s = append(s, sline)
+      } else {
+//        sput(s)
+        root := strings.Split(s[0], " | ")
+	doc := Words{0, root[0], []Dicts{}}
+	for _, str := range s {
+	  strsplit := strings.Split(strings.TrimLeft(str, " "), " | ")
+	  doc.Types = append(doc.Types, Dicts{strsplit[0], strsplit[1]})
+	  if err != nil {
+	    log.Fatal(err)
+	  }
+        s = []string{}
+        }
+	_ , err = collection.InsertOne(context.TODO(), doc)
+	if err != nil {
+          log.Fatal(err)
+        }
       }
+    }
+/*    if len(s) > 0 {
+      sput(s)
+      s = []string{}
     }
 */
     err = client.Disconnect(context.TODO())
@@ -78,3 +93,13 @@ func main() {
     fmt.Println("Connection to MongoDB closed.")
 
 }
+/*
+func sput(s1 []string){
+  root := strings.Split(s1[0], " | ")
+//  fmt.Printf("%s --- %d\n", s1[0], len(s1))
+  for i, str := range s1 {
+    strsplit := strings.Split(strings.TrimLeft(str, " "), " | ")
+    fmt.Printf("%d --- %s --- %s --- %s\n", i, root[0], strsplit[0], strsplit[1])
+  }
+}
+*/
